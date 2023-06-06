70ee79bf5da97e15749063ce662b3ef2839d9908
Michael Brunner <brunner@cadwork.swiss>
2023-06-06 13:57:02 +0200

    implement first set of functions.

--- Dimension/CDimension3dWrapper.py
+++ Dimension/CDimension3dWrapper.py
@@ -17,8 +17,7 @@ class CDimension3dWrapper:
                  aDimensionPoints: List[cadwork.point_3d]):
 
         self.__direction: cadwork.point_3d = aDirection
-        self.__plane_normal: cadwork.point_3d = aPlaneData.get_normal_vector()
-        self.__plane: cadwork.point_3d = aPlaneData.get_plane()
+        self.__plane: CPlane3D = aPlaneData
         self.__distance: cadwork.point_3d = aDistance
         self.__dimension_points: List[cadwork.point_3d] = aDimensionPoints
         self.__dimension_element_id: int = 0
@@ -28,8 +27,8 @@ class CDimension3dWrapper:
 
     def create_dimension(self):
         self.__dimension_element_id = dc.create_dimension(self.__direction,
-                                                          self.__plane_normal,
-                                                          self.__plane,
+                                                          self.__plane.get_normal(),
+                                                          self.__plane.get_plane(),
                                                           self.__distance,
                                                           self.__dimension_points)
 
@@ -37,35 +36,55 @@ class CDimension3dWrapper:
         return self.__dimension_element_id
 
     def set_dimension_element_id(self, aDimensionElementId: int):
-        if not self.__validate_element_id():
-            print(f"invalid element id: {aDimensionElementId}")
-            return
+        self.__validate_element_id()
         self.__dimension_element_id = aDimensionElementId
 
+    def get_dimension_normal(self) -> cadwork.point_3d:
+        return self.__plane.get_normal()
+
+    def get_dimension_plane(self) -> cadwork.point_3d:
+        return self.__plane.get_plane()
+
     def set_orientation(self, aViewDirection: cadwork.point_3d, aViewUpDirection: cadwork.point_3d):
-        if not self.__validate_element_id():
-            print(f"invalid element id: {self.__dimension_element_id}")
-            return
+        self.__validate_element_id()
         print(f"set orientation for dimension {self.__dimension_element_id}")
         dc.set_orientation([self.__dimension_element_id], aViewDirection, aViewUpDirection)
 
     def add_segment(self, aDimensionSegment: cadwork.point_3d):
-        if not self.__validate_element_id():
-            print(f"invalid element id: {self.__dimension_element_id}")
-            return
+        self.__validate_element_id()
         dc.add_segment(self.__dimension_element_id, aDimensionSegment)
 
-    def __validate_element_id(self) -> bool:
-        return ec.check_element_id(self.__dimension_element_id)
+    def __validate_element_id(self):
+        """Check if element id is valid
+        :raises ValueError: if element id is invalid
+        handle in production code with try/except"""
+        if not ec.check_element_id(self.__dimension_element_id):
+            raise ValueError(f"invalid element id: {self.__dimension_element_id}")
 
     def set_unit_precision(self, aPrecision: int = 1):
-        if not self.__validate_element_id():
-            print(f"invalid element id: {self.__dimension_element_id}")
-            return
+        self.__validate_element_id()
         dc.set_precision([self.__dimension_element_id], aPrecision)
 
     def set_text_size(self, aTextSize: float):
-        if not self.__validate_element_id():
-            print(f"invalid element id: {self.__dimension_element_id}")
-            return
+        self.__validate_element_id()
         dc.set_text_size([self.__dimension_element_id], aTextSize)
+
+    def set_line_thickness(self, aLineThickness: float):
+        self.__validate_element_id()
+        dc.set_line_thickness([self.__dimension_element_id], aLineThickness)
+
+    def set_total_dimension(self, aTotalDimension: bool = True):
+        self.__validate_element_id()
+        dc.set_total_dimension([self.__dimension_element_id], aTotalDimension)
+
+    def set_text_color(self, aColorNr: int):
+        self.__validate_element_id()
+        dc.set_text_color([self.__dimension_element_id], aColorNr)
+
+    def set_line_color(self, aColorNr: int):
+        self.__validate_element_id()
+        dc.set_line_color([self.__dimension_element_id], aColorNr)
+
+    def set_default_anchor_length(self, aAnchorLength: float):
+        self.__validate_element_id()
+        dc.set_default_anchor_length([self.__dimension_element_id], aAnchorLength)
